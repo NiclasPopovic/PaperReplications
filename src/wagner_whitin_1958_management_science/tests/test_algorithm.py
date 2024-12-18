01e053eb35dc5c82006c283a9cbd9d714ad65d6c
Niclas Popovic <73672225+NiclasPopovic@users.noreply.github.com>
2024-12-18 20:39:33 +0100

    UPDATED: test_algorithm.py added test

--- src/wagner_whitin_1958_management_science/tests/test_algorithm.py
+++ src/wagner_whitin_1958_management_science/tests/test_algorithm.py
@@ -1,12 +1,18 @@
-import unittest
+from unittest import TestCase
+from src.wagner_whitin_1958_management_science.source.dynamic_program import DynamicProgram
 
-
-
-class TestAlgorithm(unittest.TestCase):
+class TestAlgorithm(TestCase):
 
     def setUp(self):
-        pass
+        self.DPAlgorithm = DynamicProgram(demand=[69, 29, 36, 61, 61, 26,
+                                                  34, 67,  45, 67, 79, 56],
+                                          interest_charge=1,
+                                          ordering_cost=[85, 102, 102, 101, 98, 114,
+                                                  105, 86, 119, 110, 98, 114])
+
 
     def testAlgorithm(self):
-        pass
+        self.DPAlgorithm.run()
+        self.assertEqual(864, self.DPAlgorithm.period_costs[12])
+
 
