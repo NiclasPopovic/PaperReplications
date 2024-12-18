5e8fc51d9c8c7e2336f03098796e7cefe9e4f6ff
Niclas Popovic <73672225+NiclasPopovic@users.noreply.github.com>
2024-12-18 20:40:35 +0100

    Small fixes

--- src/wagner_whitin_1958_management_science/code/dynamic_program.py
+++
@@ -1,8 +0,0 @@
-
-
-class DynamicProgram():
-
-    def __init__(self, demand, interest_charge, ordering_cost):
-        self._demand = demand
-        self._interest_charge = interest_charge
-        self._ordering_cost = ordering_cost
--- src/wagner_whitin_1958_management_science/code/main.py
+++
@@ -1,9 +0,0 @@
-
-from dynamic_program import DynamicProgram
-
-
-if __name__ == '__main__':
-    try:
-        DP = DynamicProgram()
-    except Exception as e:
-        print(f"An error occurred: {e}")
