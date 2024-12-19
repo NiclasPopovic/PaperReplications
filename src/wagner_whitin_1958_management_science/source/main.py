1512aee0169e254e29921e076a977193afd45ec5
Niclas Popovic <73672225+NiclasPopovic@users.noreply.github.com>
2024-12-19 08:54:16 +0100

    ADDED: DP to main.py

--- src/wagner_whitin_1958_management_science/source/main.py
+++ src/wagner_whitin_1958_management_science/source/main.py
@@ -1,9 +1,12 @@
-
 from dynamic_program import DynamicProgram
-
 
 if __name__ == '__main__':
     try:
-        DP = DynamicProgram()
+        DP = DynamicProgram(demand=[69, 29, 36, 61, 61, 26,
+                                    34, 67, 45, 67, 79, 56],
+                                    interest_charge=1,
+                                    ordering_cost=[85, 102, 102, 101, 98, 114,
+                                                105, 86, 119, 110, 98, 114])
+        DP.run()
     except Exception as e:
         print(f"An error occurred: {e}")
